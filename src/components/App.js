e3888968034a9c6513ad7eb6034f629de2992318
RavenVol <r.volianiuk@gmail.com>
2019-08-29 22:20:07 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import '../styles/App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    )
+  }
+}
+
+export default App;
